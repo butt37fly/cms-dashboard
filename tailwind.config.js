ac44201f2bf761a3e05cd52fb8347d9e40a0cfb0
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-22 12:11:59 -0500

    feat: añadir colores del tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#BFDBFE',
+          100: '#326383',
+          200: '#154C6B',
+          300: '#1F2937',
+        },
+        secondary: {
+          50: '#E2C8EE',
+          100: '#9875A8',
+          200: '#784E8B',
+          300: '#603574',
+        },
+        alert: {
+          50: '#FFF4B7',
+          100: '#EDDA6F',
+          200: '#E8D14F',
+          300: '#D0B620',
+        },
+        success: {
+          50: '#C3FFA8',
+          100: '#7ED756',
+          200: '#61A442',
+          300: '#44762D',
+        },
+        danger: {
+          50: '#FBAEAF',
+          100: '#DD6769',
+          200: '#B24749',
+          300: '#762D2E',
+        },
+      },
+    },
   },
   plugins: [],
 }
